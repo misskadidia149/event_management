78d77db77c348e606046b480898c631b679d6223
Siby1100 <Soule1716@gmail.com>
2025-02-18 00:49:13 +0000

    page de connexion et de deconnexion

--- pages/login.php
+++ pages/login.php
@@ -1,1 +1,91 @@
 <!-- Page de connexion -->
+<?php
+session_start();
+require_once "../config/db.php";
+$message = "";
+
+if (isset($_POST['login'])) {
+    $email = trim($_POST['email']);
+    $password = trim($_POST['mdp']);
+
+    if (!empty($email) && !empty($password)) {
+        $verifyUser = $pdo->prepare("SELECT * FROM users WHERE email = ?");
+        $verifyUser->execute([$email]);
+        $user = $verifyUser->fetch();
+
+        if ($user && password_verify($password, $user['mot_de_passe'])) {
+            $_SESSION['user_id'] = $user['id']; // Stocke l'ID de l'utilisateur en session
+            $_SESSION['email'] = $user['email'];
+            header("Location: index.php"); // Redirige après connexion
+            exit();
+        } else {
+            $message = "<div class='alert alert-danger text-center'>Mauvais identifiants !</div>";
+        }
+    } else {
+        $message = "<div class='alert alert-danger text-center'>Tous les champs doivent être remplis !</div>";
+    }
+}
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Bootstrap Icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="left-section">
+        </div>
+        <div class="right-section">
+            <div class="login-box">
+                <div class="logo">
+                    <h1 class="fw-semibold">Connexion</h1>
+                </div>
+                <form action="login.php" method="post">
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">
+                            <i class="bi bi-envelope-check"></i>
+                        </span>
+                        <input type="email" class="form-control" placeholder="Votre adresse email" name="email"
+                            aria-label="Username" aria-describedby="basic-addon1" required>
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">
+                            <i class="bi bi-shield-check"></i>
+                        </span>
+                        <input type="password" class="form-control" placeholder="Votre mot de passe" name="mdp"
+                            aria-label="Username" aria-describedby="basic-addon1" required>
+                    </div>
+                    <div>
+                        <?php
+                        echo $message;
+                        ?>
+                    </div>
+                    <button class="login-btn btn btn-primary" name="login">Se connecter</button>
+                    <a href="register.php" class="forgot-password">Inscrivez vous !</a>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+
+</body>
+
+</html>
--- pages/logout.php
+++ pages/logout.php
@@ -1,1 +1,10 @@
 <!-- page de deconnexion -->
+
+<?php
+session_start();
+session_unset(); // Supprime toutes les variables de session
+session_destroy(); // Détruit la session
+
+header("Location: login.php"); // Redirige vers la page de connexion
+exit();
+?>
